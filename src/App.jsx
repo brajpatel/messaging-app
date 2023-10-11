cef520ecb84e8d75a1608970637946e1ee1bcc5d
brajpatel <brajeshpatel0002@gmail.com>
2023-10-11 17:02:11 +0100

    Finish setting up app with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className='container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10'>
-      <p className='text-3xl text-gray-700 font-bold mb-5'>Tailwind Test</p>
-      <p className='text-gray-500 text-lg'>Chlay, and dree.</p>
+      <h1 className='text-xl text-blue-900 font-bold'>Tailwind Test</h1>
     </div>
   )
 }
