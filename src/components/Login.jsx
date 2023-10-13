fc7fea4d6eefd13f3638498e49b7a90775f225bf
brajpatel <brajeshpatel0002@gmail.com>
2023-10-13 09:30:58 +0100

    Create login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import './Login.css';
+
+function Login() {
+    return (
+        <div>
+            <p className='bg-gray-900 text-white p-4 rounded-2xl shadow'>LOGIN</p>
+        </div>
+    )
+}
+
+export default Login;
