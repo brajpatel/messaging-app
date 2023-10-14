9e5089bfb420195dd620fd22d9039356c21207d4
brajpatel <brajeshpatel0002@gmail.com>
2023-10-14 20:32:48 +0100

    Use rose as primary color

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,34 @@
 function Login() {
     return (
-        <section class="bg-gray-50 dark:bg-gray-900">
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="#" class="flex items-center mb-2 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img class="w-20 h-20" src="/logo.png" alt="logo"/>
-                    Macaw
-                </a>
-                <div class="bg-white dark:bg-gray-700 shadow p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 className='font-bold'>
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex items-center justify-center gap-60 px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="flex flex-col">
+                    <a href="#" className="flex items-center mb-2 text-2xl font-semibold text-gray-900 dark:text-white">
+                        <img className="w-20 h-20" src="/logo.png" alt="logo"/>
+                        Macaw
+                    </a>
+                    <div>
+                        
+                    </div>
+                </div>
+
+                <div className="bg-white dark:bg-gray-700 shadow p-6 space-y-4 md:space-y-6 sm:p-8 rounded-lg">
+                    <h1 className='font-bold dark:text-gray-100'>
                         Sign in to your account
                     </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
+                    <form className="space-y-4 md:space-y-6" action="#">
                         <div>
-                            <label for="email">Your email</label>
-                            <input className='block' type="email" name="email" id="email" placeholder="name@company.com" required=""/>
+                            <label className="mb-2" htmlFor="email">Your email</label>
+                            <input className='block w-full px-4 py-2 border-2 focus:outline-none focus:border-rose-500 rounded-md' type="email" name="email" id="email" placeholder="name@company.com" required=""/>
                         </div>
                         <div>
-                            <label for="password">Password</label>
-                            <input className='block' type="password" name="password" id="password" placeholder="********" required=""/>
+                            <label className="mb-2" htmlFor="password">Password</label>
+                            <input className='block w-full px-4 py-2 border-2 focus:outline-none focus:border-rose-500 rounded-md' type="password" name="password" id="password" placeholder="••••••••••••" required=""/>
                         </div>
                         <div>
                             <a href="#">Forgot password?</a>
                         </div>
-                        <button type="submit">Sign in</button>
+                        <button className="bg-rose-600 text-white w-full py-2 rounded-md" type="submit">Sign in</button>
                         <p>
                             Don't have an account yet? <a href="#">Sign up</a>
                         </p>
