621f59483f87f3ba2db3d7d39da2021359502aae
brajpatel <brajeshpatel0002@gmail.com>
2023-10-16 14:44:17 +0100

    Remove app features section

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,41 +2,12 @@ function Login() {
     return (
         <section className="bg-gray-50 dark:bg-gray-900 h-screen">
             <div className="flex items-center justify-center gap-10 px-6 mx-auto lg:py-0">
-                <div className="hidden md:flex flex-col items-center md:w-1/2 lg:w-1/3">
-                    <a href="https://github.com/brajpatel" target="_blank" className="flex items-center text-2xl font-semibold text-gray-900 dark:text-gray-50">
-                        <img className="w-32 h-32" src="/logo.png" alt="logo"/>
-                        Macaw
-                    </a>
-                
-                    <div className="flex flex-col gap-5">
-                        <div className="flex gap-2">
-                            <i className="fa-solid fa-circle-check text-xl text-rose-600"></i>
-                            <div className="flex flex-col">
-                                <p className="font-bold text-lg lg:text-xl dark:text-gray-200">Make connections</p>
-                                <p className="lg:text-lg dark:text-gray-300">Connect with others from around the world!</p>
-                            </div>
-                        </div>
-
-                        <div className="flex gap-2">
-                            <i className="fa-solid fa-circle-check text-xl text-rose-600"></i>
-                            <div className="flex flex-col">
-                                <p className="font-bold text-lg lg:text-xl dark:text-gray-200">Another feature</p>
-                                <p className="lg:text-lg dark:text-gray-300">Description for this feature</p>
-                            </div>
-                        </div>
-
-                        <div className="flex gap-2">
-                            <i className="fa-solid fa-circle-check text-xl text-rose-600"></i>
-                            <div className="flex flex-col">
-                                <p className="font-bold text-lg lg:text-xl dark:text-gray-200">Another feature</p>
-                                <p className="lg:text-lg dark:text-gray-300">Description for this feature</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
                 <div className="bg-white dark:bg-gray-800 shadow p-6 space-y-4 w-full mt-8 md:w-1/2 md:mt-12 lg:w-1/3 md:space-y-6 sm:p-8 rounded-lg">
-                    <h1 className='font-bold dark:text-gray-100 text-xl text-center underline'>Sign in to your account</h1>
+                    <h1 className='font-bold dark:text-gray-100 text-xl text-center'>
+                        Welcome back!
+                        <span className="block dark:text-gray-300">We're so excited to see you again!</span>
+                    </h1>
+
                     <form className="space-y-4 md:space-y-6" action="#">
                         <div>
                             <label className="text-lg dark:text-gray-100" htmlFor="email">Your email</label>
