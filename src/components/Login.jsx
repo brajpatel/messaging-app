5bf3ddeddad2e6143c853740bc1ce78f1478462a
brajpatel <brajeshpatel0002@gmail.com>
2023-10-17 11:13:09 +0100

    Adjust login modal on different screen sizes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,7 @@ function Login() {
     return (
         <section className="bg-gray-50 dark:bg-gray-900 h-screen">
             <div className="flex items-center justify-center gap-10 px-6 mx-auto lg:py-0">
-                <div className="bg-white dark:bg-gray-800 shadow-lg p-6 space-y-4 w-full mt-8 md:w-1/2 md:mt-12 lg:w-1/3 md:space-y-6 sm:p-8 rounded-lg">
+                <div className="bg-white dark:bg-gray-800 shadow-lg px-6 pb-6 space-y-4 w-full mt-8 md:w-3/4 lg:w-1/2 xl:w-1/3 md:mt-12 md:space-y-6 rounded-lg">
                     <a href="https://github.com/brajpatel" target="_blank" className="flex flex-col items-center justify-center text-2xl font-semibold text-gray-900 dark:text-gray-50">
                         <img className="w-40 h-40" src="/logo.png" alt="logo"/>
                     </a>
