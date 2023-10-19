7b05ae91aae5c88674c29a83aadc315d4e4defea
brajpatel <brajeshpatel0002@gmail.com>
2023-10-19 15:58:51 +0100

    Create sidebar layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,25 @@
 function Sidebar() {
     return (
-        <nav>
-            Sidebar
+        <nav className="h-screen w-1/4 bg-rose-600 text-zinc-50">
+            <ul>
+                <li>Hello, [user] --------------------------------------------------- Profile picture</li>
+                <li>.</li>
+                <li>DROPDOWN</li>
+                <li>Profile</li>
+                <li>Theme</li>
+                <li>Logout</li>
+                <li>.</li>
+                <li>Friends</li>
+                <li>.</li>
+                <li>If no friends</li>
+                <li>No friends yet, add some</li>
+                <li>.</li>
+                <li>else</li>
+                <li>Friend profile picture ----- friend name
+                    <span className="block opacity-90">status message</span>
+                </li>
+                <li>SEARCH (goes to search page)</li>
+            </ul>
         </nav>
     )
 }
