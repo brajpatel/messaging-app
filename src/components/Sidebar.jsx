7dd2eadb827fcb473ecdbf6ee72d58ec4d00a4a0
brajpatel <brajeshpatel0002@gmail.com>
2023-10-20 17:49:01 +0100

    Adjust border widths

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,7 +25,7 @@ function Sidebar({ theme, handleTheme }) {
                 <p className="text-zinc-50 text-xl">Greetings, <span className="font-bold">[USER]</span>.</p>
                 <div className="h-16 w-16 bg-zinc-300 border-2 border-zinc-50 rounded-full cursor-pointer" onClick={handleDropdown}></div>
 
-                <div className="dropdown transition-all w-full absolute top-full right-full flex flex-col bg-white border-2 border-rose-600 dark:border-zinc-800 text-lg shadow overflow-hidden z-10">
+                <div className="dropdown transition-all w-full absolute top-full right-full flex flex-col pt-px bg-white dark:bg-zinc-700 border-x-0 border-y border-rose-600 dark:border-x-0 dark:border-y dark:border-zinc-700 text-lg shadow overflow-hidden z-10">
                     <p className="h-full w-full px-4 py-2 text-center hover:bg-rose-600 hover:text-zinc-50 dark:bg-zinc-800 dark:hover:bg-zinc-700 dark:text-zinc-50 cursor-pointer" onClick={handleTheme}>
                         Change Theme
                         { theme === 'light' ?
@@ -34,10 +34,16 @@ function Sidebar({ theme, handleTheme }) {
                             <i className="fa-regular fa-sun ml-2"></i>
                         }
                     </p>
-                    <Link to="/user_name" className="h-full w-full px-4 py-2 text-center hover:bg-rose-600 border-y-2 border-rose-600 dark:border-zinc-700 hover:text-zinc-50 dark:bg-zinc-800 dark:hover:bg-zinc-700 dark:text-zinc-50 cursor-pointer" onClick={handleDropdown}>
+
+                    <hr className="border-1 border-rose-600 dark:border-zinc-700"/>
+
+                    <Link to="/user_name" className="h-full w-full px-4 py-2 text-center hover:bg-rose-600 hover:text-zinc-50 dark:bg-zinc-800 dark:hover:bg-zinc-700 dark:text-zinc-50 cursor-pointer" onClick={handleDropdown}>
                         Profile
                         <i className="fa-regular fa-user ml-2"></i>
                     </Link>
+                    
+                    <hr className="border-1 border-rose-600 dark:border-zinc-700"/>
+                    
                     <p className="h-full w-full px-4 py-2 text-center hover:bg-rose-600 hover:text-zinc-50 dark:bg-zinc-800 dark:hover:bg-zinc-700 dark:text-zinc-50 cursor-pointer">
                         Logout
                         <i className="fa-solid fa-door-open ml-2"></i>
