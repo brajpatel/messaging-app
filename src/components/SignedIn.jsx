53160d809fb498ae0eccfe317fc3262801ba539d
brajpatel <brajeshpatel0002@gmail.com>
2023-10-24 20:21:37 +0100

    Create component to display on sign in

---
+++ src/components/SignedIn.jsx
@@ -0,0 +1,10 @@
+function SignedIn() {
+    return (
+        <div>
+            <p>A quote just for you:</p>
+            <p>That which is dreamed cannot be grasped. But work toward it day to day and you will find it in your hands.</p>
+        </div>
+    )
+}
+
+export default SignedIn;
