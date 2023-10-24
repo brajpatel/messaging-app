0865d56148f6d368df439ee3c13a33c353d1a439
brajpatel <brajeshpatel0002@gmail.com>
2023-10-24 18:02:30 +0100

    Adjust card hover to change border color and no longer has a box shadow

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -34,7 +34,7 @@ function Sidebar({ theme, handleTheme }) {
 
     return (
         <nav className="h-screen w-full lg:w-96 float-left flex flex-col bg-white dark:bg-gray-800 lg:border-r-2 border-rose-600 dark:border-gray-700">
-            <div className="h-[4.4rem] md:h[4.6rem] lg:h-20 relative flex justify-between items-center px-6 md:py-6 lg:py-4 bg-rose-600 dark:bg-gray-700">
+            <div className="h-[4.4rem] md:h[4.6rem] lg:h-20 relative flex justify-between items-center px-6 md:py-6 lg:py-3 xl:py-4 bg-rose-600 dark:bg-gray-700">
                 <p className="text-gray-50 text-lg lg:text-xl">Greetings, <span className="font-bold">[USER]</span>.</p>
                 <div className="h-14 w-14 md:h-[3.75rem] md:w-[3.75rem] lg:h-16 lg:w-16 rounded-full bg-gray-300 dark:bg-gray-500 border-2 border-gray-50 cursor-pointer" onClick={handleDropdown}></div>
 
@@ -70,7 +70,7 @@ function Sidebar({ theme, handleTheme }) {
                     <ul className="h-full w-full px-4 py-5 flex flex-col gap-3 sm:grid sm:grid-cols-2 sm:auto-rows-max lg:flex lg:flex-col lg:flex-nowrap overflow-auto">
                         {friends.map((friend, index) => {
                             return (
-                                <li key={index} className="w-full sm:w-auto lg:w-full rounded-lg flex gap-8 px-4 py-3 md:py-4 bg-white dark:bg-gray-700 border border-rose-600 dark:border-gray-800 transition-all hover:border-white dark:hover:border-gray-700 hover:shadow-[0_2px_6px_rgb(190,190,190)] dark:hover:shadow-[0_2px_6px_rgb(30,30,30)] cursor-pointer">
+                                <li key={index} className="w-full sm:w-auto lg:w-full rounded-lg flex gap-8 px-4 py-3 md:py-4 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-800 transition-all hover:border-rose-600 dark:hover:border-gray-500 cursor-pointer">
                                     <img className="h-10 w-10 lg:h-16 lg:w-16 bg-gray-100 dark:bg-gray-500 border border-rose-600 dark:border-gray-800 rounded-full" src={friend.profile_picture} alt={`${friend.username}'s profile picture`}/>
                                     <div>
                                         <p className="lg:text-lg font-bold">{friend.username}</p>
