a1c8e6a955e8a9a9bb97a9df9c645583da4bf6b3
brajpatel <brajeshpatel0002@gmail.com>
2023-10-25 09:12:37 +0100

    Hide x overflow and make y overflow auto

--- src/components/SignedIn.jsx
+++ src/components/SignedIn.jsx
@@ -1,6 +1,8 @@
 function SignedIn() {
     return (
-        <div className='h-auto lg:h-full w-auto overflow-hidden bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-50'>
+        <div className='h-auto lg:h-full w-auto overflow-x-hidden overflow-y-auto p-4 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-50'>
+            <p className="text-xl font-bold">Something interactive, a randome quote generator - fill up this space</p>
+
             <p>A quote just for you:</p>
             <p>That which is dreamed cannot be grasped. But work toward it day to day and you will find it in your hands.</p>
         </div>
