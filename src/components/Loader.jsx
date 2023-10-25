761337337a1944951330a3fb64b82a5f65c03e19
brajpatel <brajeshpatel0002@gmail.com>
2023-10-25 17:20:51 +0100

    Create loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import './Loader.css';
+
+function Loader() {
+    return (
+        <div className='loader-container'>
+            <div className="loader-bot"></div>
+            <div className="loader-top-1"></div>
+            <div className="loader-top-2"></div>
+        </div>
+    )
+}
+
+export default Loader;
