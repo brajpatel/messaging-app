8e45a4c9710532c3af23a1e0253def864cfde349
brajpatel <brajeshpatel0002@gmail.com>
2023-10-25 10:15:51 +0100

    Make profile page position relatively on larger screens

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,9 +3,10 @@ import { AiOutlineArrowLeft } from 'react-icons/ai';
 
 function Profile() {
     return (
-        <div className='h-screen lg:h-full w-full lg:w-auto absolute top-0 right-0 bg-white dark:bg-gray-800 overflow-x-hidden overflow-y-auto'>
+        <div className='h-screen lg:h-full w-full lg:w-auto absolute top-0 right-0 lg:relative bg-white dark:bg-gray-800 overflow-x-hidden overflow-y-auto'>
+
             <div className="h-[4.4rem] font-bold bg-rose-600 dark:bg-gray-700 text-gray-50 flex justify-between items-center px-4">
-                <Link to="/" className='hover:bg-[rgb(255,255,255,0.4)] dark:hover:bg-[rgb(255,255,255,0.2)] p-2 rounded-full'>
+                <Link to="/" className='block lg:hidden hover:bg-[rgb(255,255,255,0.4)] dark:hover:bg-[rgb(255,255,255,0.2)] p-2 rounded-full'>
                     <AiOutlineArrowLeft className='text-2xl cursor-pointer'/>
                 </Link>
 
