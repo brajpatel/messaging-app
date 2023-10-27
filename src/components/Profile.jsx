4ce271487055a7ddb425877d2d9ea0620b9f475d
brajpatel <brajeshpatel0002@gmail.com>
2023-10-27 17:34:48 +0100

    Adjust heading section background color

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,16 +3,16 @@ import { AiOutlineArrowLeft } from 'react-icons/ai';
 
 function Profile() {
     return (
-        <div className='h-screen lg:h-full w-full lg:w-auto absolute top-0 right-0 lg:relative bg-white dark:bg-gray-800 overflow-x-hidden overflow-y-auto'>
+        <div className='h-screen lg:h-full w-full lg:w-auto absolute top-0 left-0 lg:relative bg-white dark:bg-gray-800 overflow-x-hidden overflow-y-auto'>
 
-            <div className="h-[4.4rem] font-bold bg-rose-600 dark:bg-gray-700 text-gray-50 flex justify-between items-center px-4">
-                <Link to="/" className='block lg:hidden hover:bg-[rgb(255,255,255,0.4)] dark:hover:bg-[rgb(255,255,255,0.2)] p-2 rounded-full'>
-                    <AiOutlineArrowLeft className='text-2xl cursor-pointer'/>
-                </Link>
+        <div className="h-[4.4rem] md:h-[4.6rem] font-bold bg-rose-600 lg:bg-white dark:bg-gray-700 lg:dark:bg-gray-800 text-gray-50 lg:text-gray-900 dark:text-gray-50 flex justify-between items-center px-4">
+            <Link to="/" className='block lg:hidden hover:bg-[rgb(255,255,255,0.4)] dark:hover:bg-[rgb(255,255,255,0.2)] p-2 rounded-full'>
+                <AiOutlineArrowLeft className='text-2xl cursor-pointer'/>
+            </Link>
 
-                <p>Your Profile</p>
-            </div>
+            <p className='text-lg'>Profile</p>
         </div>
+    </div>
     )
 }
 
--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -5,12 +5,12 @@ function SearchPage() {
     return (
         <div className='h-screen lg:h-full w-full lg:w-auto absolute top-0 left-0 lg:relative bg-white dark:bg-gray-800 overflow-x-hidden overflow-y-auto'>
 
-        <div className="h-[4.4rem] font-bold bg-rose-600 dark:bg-gray-700 text-gray-50 flex justify-between items-center px-4">
+        <div className="h-[4.4rem] md:h-[4.6rem] font-bold bg-rose-600 lg:bg-white dark:bg-gray-700 lg:dark:bg-gray-800 text-gray-50 lg:text-gray-900 dark:text-gray-50 flex justify-between items-center px-4">
             <Link to="/" className='block lg:hidden hover:bg-[rgb(255,255,255,0.4)] dark:hover:bg-[rgb(255,255,255,0.2)] p-2 rounded-full'>
                 <AiOutlineArrowLeft className='text-2xl cursor-pointer'/>
             </Link>
 
-            <p>Search</p>
+            <p className='text-lg'>Search</p>
         </div>
     </div>
     )
