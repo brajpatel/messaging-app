9554776f7e3f3306adae173c4ba3a798914693d4
brajpatel <brajeshpatel0002@gmail.com>
2023-10-27 10:37:43 +0100

    Add link to home page

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,8 +1,23 @@
 import { Link } from "react-router-dom";
+import { BsHouseDoor } from 'react-icons/bs';
 
 function Dropdown({ handleDropdown, theme, handleTheme }) {
     return (
         <div className="dropdown transition-all w-full absolute top-full right-full flex flex-col pt-px bg-white dark:bg-gray-800 border-x-0 border-y border-rose-600 dark:border-x-0 dark:border-y dark:border-gray-800 text-lg shadow-md overflow-hidden z-10">
+            <Link to="/" className="hidden lg:block h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-700 dark:hover:bg-gray-800 dark:text-gray-50 cursor-pointer" onClick={handleDropdown}>
+                Home
+                <BsHouseDoor className="inline-block ml-2"/>
+            </Link>
+
+            <hr className="border-1 border-rose-600 dark:border-gray-800"/>
+
+            <Link to="/username" className="h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-700 dark:hover:bg-gray-800 dark:text-gray-50 cursor-pointer" onClick={handleDropdown}>
+                Profile
+                <i className="fa-regular fa-user ml-2"></i>
+            </Link>
+            
+            <hr className="border-1 border-rose-600 dark:border-gray-800"/>
+
             <p className="h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-700 dark:hover:bg-gray-800 dark:text-gray-50 cursor-pointer" onClick={handleTheme}>
                 Change Theme
                 { theme === 'light' ?
@@ -12,13 +27,6 @@ function Dropdown({ handleDropdown, theme, handleTheme }) {
                 }
             </p>
 
-            <hr className="border-1 border-rose-600 dark:border-gray-800"/>
-
-            <Link to="/username" className="h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-700 dark:hover:bg-gray-800 dark:text-gray-50 cursor-pointer" onClick={handleDropdown}>
-                Profile
-                <i className="fa-regular fa-user ml-2"></i>
-            </Link>
-            
             <hr className="border-1 border-rose-600 dark:border-gray-800"/>
             
             <p className="h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-700 dark:hover:bg-gray-800 dark:text-gray-50 cursor-pointer">
