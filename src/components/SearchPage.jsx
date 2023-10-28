4c2b82ced41274bb87df9cf2b382d5e4d1b8a6e3
brajpatel <brajeshpatel0002@gmail.com>
2023-10-28 17:29:34 +0100

    Create function to filter friends list based on user input

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -9,29 +9,42 @@ function SearchPage() {
         {
             username: 'Ashe',
             status_message: 'All the world on one arrow',
-            profile_picture: 'https://64.media.tumblr.com/91b159df9a8c606c4fd5a74427d2aa9c/b9116db0ab4711c9-77/s500x750/01bd6c7a8791be7f6f6e4b7afee2424cf671bd53.png'
+            profile_picture: 'https://64.media.tumblr.com/91b159df9a8c606c4fd5a74427d2aa9c/b9116db0ab4711c9-77/s500x750/01bd6c7a8791be7f6f6e4b7afee2424cf671bd53.png',
+            isFriend: false
         },
         {
             username: 'Jhin',
             status_message: 'In carnage I bloom, like a flower in the dawn',
-            profile_picture: 'https://i.pinimg.com/1200x/9d/00/4c/9d004c40630c28c54e1bc37d10b35657.jpg'
+            profile_picture: 'https://i.pinimg.com/1200x/9d/00/4c/9d004c40630c28c54e1bc37d10b35657.jpg',
+            isFriend: false
         },
         {
             username: 'Nasus',
             status_message: 'Hehe',
-            profile_picture: 'https://i.pinimg.com/564x/0e/d8/54/0ed8549cbf8d409bc3b90731085f865d.jpg'
+            profile_picture: 'https://i.pinimg.com/564x/0e/d8/54/0ed8549cbf8d409bc3b90731085f865d.jpg',
+            isFriend: false
         },
         {
             username: 'Jinx',
             status_message: 'Rules are made to be broken!',
-            profile_picture: 'https://wallpapers-clan.com/wp-content/uploads/2023/07/league-of-legends-jinx-pfp-01.jpg'
+            profile_picture: 'https://wallpapers-clan.com/wp-content/uploads/2023/07/league-of-legends-jinx-pfp-01.jpg',
+            isFriend: false
         },
         {
             username: 'Irelia',
             status_message: 'Blade dancer slay',
-            profile_picture: 'https://i.pinimg.com/564x/fd/56/3b/fd563b26fb28a70d2b262a39af322655.jpg'
+            profile_picture: 'https://i.pinimg.com/564x/fd/56/3b/fd563b26fb28a70d2b262a39af322655.jpg',
+            isFriend: false
         }
     ])
+    const [searchedFriends, setSearchedFriends] = useState(friends);
+    const [searchValue, setSearchValue] = useState('');
+
+    const handleSearch = (e) => {
+        setSearchedFriends(friends.filter((friend) => {
+            return friend.username.toLowerCase().startsWith(e.target.value)
+        }));
+    }
 
     return (
         <div className='h-screen lg:h-full w-full lg:w-auto absolute top-0 left-0 lg:relative lg:p-3 flex flex-col items-center bg-white dark:bg-gray-800 overflow-x-hidden overflow-y-auto'>
@@ -44,18 +57,16 @@ function SearchPage() {
                     <p className='block lg:hidden text-lg lg:underline'>Search</p>
 
                     <p className='hidden lg:block lg:text-xl lg:underline'>Search for friends</p>
-
-
                 </div>
 
                 <div className='w-[90%] sm:w-3/5 md:w-1/2 xl:w-1/3 mx-auto flex justify-center items-center py-4 lg:p-2'>
-                    <input className='w-full bg-white dark:bg-gray-700 text-lg text-black dark:text-gray-100 px-4 py-2 mt-2 border-2 border-gray-200 dark:border-gray-700 focus:outline-none focus:border-rose-500 dark:focus:border-gray-400 rounded text-center' placeholder="Search by username"/>
+                    <input className='w-full bg-white dark:bg-gray-700 text-lg text-black dark:text-gray-100 px-4 py-2 mt-2 border-2 border-gray-200 dark:border-gray-700 focus:outline-none focus:border-rose-500 dark:focus:border-gray-400 rounded text-center' placeholder="Search by username" onInput={handleSearch}/>
                 </div>
             </div>
 
             <div className='w-full'>
                 <ul className="h-full w-full dark:text-gray-50 px-4 py-5 grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-4 overflow-x-hidden overflow-y-auto">
-                        {friends.map((friend, index) => {
+                        {searchedFriends.map((friend, index) => {
                             return (
                                 <li key={index} className="relative w-full sm:w-auto lg:w-full rounded flex gap-6 px-4 pr-14 py-3 md:py-4 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-800 transition-all hover:border-rose-600 dark:hover:border-gray-400">
                                     <div className={`h-14 w-14 md:h-[3.75rem] md:w-[3.75rem] lg:h-16 lg:w-16 bg-cover bg-center border border-rose-600 dark:border-gray-400 rounded-full overflow-hidden`} style={{backgroundImage: `url(${friend.profile_picture})`}}></div>
@@ -65,13 +76,13 @@ function SearchPage() {
                                         <p className="opacity-80 overflow-hidden whitespace-nowrap text-ellipsis">{friend.status_message}</p>
                                     </div>
 
-                                    {null ? (
-                                        <div className='bg-red-500 hover:bg-red-400 dark:bg-red-600 dark:hover:bg-red-700 text-gray-50 text-lg lg:text-xl p-1 absolute top-1/2 right-4 translate-y-[-50%] rounded-full cursor-pointer'>
-                                            <AiOutlineUserAdd/>
-                                        </div>
-                                        ) : (
+                                    {friend.isFriend ? (
                                         <div className='bg-red-500 hover:bg-red-400 dark:bg-red-600 dark:hover:bg-red-700 text-gray-50 text-lg lg:text-xl p-1 absolute top-1/2 right-4 translate-y-[-50%] rounded-full cursor-pointer'>
                                             <AiOutlineUserDelete/>
+                                        </div>
+                                        ) : (
+                                        <div className='bg-green-400 hover:bg-green-300 dark:bg-green-500 dark:hover:bg-green-600 text-gray-50 text-lg lg:text-xl p-1 absolute top-1/2 right-4 translate-y-[-50%] rounded-full cursor-pointer'>
+                                            <AiOutlineUserAdd/>
                                         </div>
                                     )}
                                 </li>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -60,7 +60,7 @@ function Sidebar({ theme, handleTheme }) {
             </div>
 
             <div className="h-[10%] flex items-center justify-center border-t-2 border-rose-600 dark:border-gray-700">
-                <Link to="/search" className="bg-rose-600 hover:bg-rose-500 dark:bg-gray-700 dark:hover:bg-gray-600 px-4 py-2 text-gray-50 rounded-lg text-lg">
+                <Link to="/search" className="bg-rose-600 hover:bg-rose-500 dark:bg-gray-700 dark:hover:bg-gray-600 px-4 py-2 text-gray-50 rounded-lg text-lg" onClick={() => setShowDropdown(false)}>
                     Search
                     <i className="fa-solid fa-magnifying-glass ml-2"></i>
                 </Link>
