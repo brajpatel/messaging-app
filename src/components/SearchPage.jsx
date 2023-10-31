93b4acab066e7d41bb02641750cac2b6f6a88f1c
brajpatel <brajeshpatel0002@gmail.com>
2023-10-31 14:58:48 +0000

    Create searched friend component

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AiOutlineArrowLeft } from 'react-icons/ai';
-import { AiOutlineUserAdd } from 'react-icons/ai';
-import { AiOutlineUserDelete } from 'react-icons/ai';
+import SearchedFriend from './SearchedFriend';
 
 function SearchPage() {
     const [friends, setFriends] = useState([
@@ -68,24 +67,7 @@ function SearchPage() {
                         <ul className="h-full w-full dark:text-gray-50 px-4 py-5 grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-4 overflow-x-hidden overflow-y-auto">
                             {searchedFriends.map((friend, index) => {
                                 return (
-                                    <li key={index} className="relative w-full sm:w-auto rounded flex gap-6 px-4 pr-14 py-3 md:py-4 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-800 transition-all hover:border-rose-600 dark:hover:border-gray-400">
-                                        <div className='h-14 min-w-[3.5rem] md:h-[3.75rem] md:min-w-[3.75rem] lg:h-16 lg:min-w-[4rem] bg-cover bg-center border border-rose-600 dark:border-gray-400 rounded-full float-left overflow-hidden' style={{backgroundImage: `url(${friend.profile_picture})`}}></div>
-
-                                        <div className='float-none overflow-hidden'>
-                                            <p className="text-[1.1rem] lg:text-lg font-bold">{friend.username}</p>
-                                            <p className="opacity-80 overflow-hidden whitespace-nowrap text-ellipsis">{friend.status_message}</p>
-                                        </div>
-
-                                        {friend.isFriend ? (
-                                            <div className='bg-red-500 hover:bg-red-400 dark:bg-red-600 dark:hover:bg-red-700 text-gray-50 text-lg lg:text-xl p-1 absolute top-1/2 right-4 translate-y-[-50%] rounded-full cursor-pointer'>
-                                                <AiOutlineUserDelete/>
-                                            </div>
-                                            ) : (
-                                            <div className='bg-green-500 hover:bg-green-400 dark:bg-green-500 dark:hover:bg-green-600 text-gray-50 text-lg lg:text-xl p-1 absolute top-1/2 right-4 translate-y-[-50%] rounded-full cursor-pointer'>
-                                                <AiOutlineUserAdd/>
-                                            </div>
-                                        )}
-                                    </li>
+                                    <SearchedFriend key={index} friend={friend}/>
                                 )
                             })}
                         </ul>
---
+++ src/components/SearchedFriend.jsx
@@ -0,0 +1,27 @@
+import { AiOutlineUserAdd } from 'react-icons/ai';
+import { AiOutlineUserDelete } from 'react-icons/ai';
+
+function SearchedFriend({ friend }) {
+    return (
+        <li className="relative w-full sm:w-auto rounded flex gap-6 px-4 pr-14 py-3 md:py-4 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-800 transition-all hover:border-rose-600 dark:hover:border-gray-400">
+            <div className='h-14 min-w-[3.5rem] md:h-[3.75rem] md:min-w-[3.75rem] lg:h-16 lg:min-w-[4rem] bg-cover bg-center border border-rose-600 dark:border-gray-400 rounded-full float-left overflow-hidden' style={{backgroundImage: `url(${friend.profile_picture})`}}></div>
+
+            <div className='float-none overflow-hidden'>
+                <p className="text-[1.1rem] lg:text-lg font-bold">{friend.username}</p>
+                <p className="opacity-80 overflow-hidden whitespace-nowrap text-ellipsis">{friend.status_message}</p>
+            </div>
+
+            {friend.isFriend ? (
+                <div className='bg-red-500 hover:bg-red-400 dark:bg-red-600 dark:hover:bg-red-700 text-gray-50 text-lg lg:text-xl p-1 absolute top-1/2 right-4 translate-y-[-50%] rounded-full cursor-pointer'>
+                    <AiOutlineUserDelete/>
+                </div>
+                ) : (
+                <div className='bg-green-500 hover:bg-green-400 dark:bg-green-500 dark:hover:bg-green-600 text-gray-50 text-lg lg:text-xl p-1 absolute top-1/2 right-4 translate-y-[-50%] rounded-full cursor-pointer'>
+                    <AiOutlineUserAdd/>
+                </div>
+            )}
+        </li>
+    )
+}
+
+export default SearchedFriend;
