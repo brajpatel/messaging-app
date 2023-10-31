c29cf6afaa177f2eead513caeb029bf8ce3a8393
brajpatel <brajeshpatel0002@gmail.com>
2023-10-31 17:46:28 +0000

    Adjust items to fit darkened background

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -4,25 +4,25 @@ import { BsHouseDoor } from 'react-icons/bs';
 function Dropdown({ showDropdown, handleDropdown, theme, handleTheme }) {
     return (
         <div className={showDropdown ?
-            'transition-all w-full absolute top-full right-0 flex flex-col pt-px bg-white dark:bg-gray-800 border-x-0 border-y border-rose-600 dark:border-x-0 dark:border-y dark:border-gray-800 text-lg shadow-md overflow-hidden z-10'
+            'transition-all w-full absolute top-full right-0 flex flex-col pt-px bg-white dark:bg-gray-900 border-x-0 border-y border-rose-600 dark:border-x-0 dark:border-y dark:border-gray-800 text-lg shadow-[0_8px_10px_rgba(0,0,0,0.5)] overflow-hidden z-10'
             :
-            'transition-all w-full absolute top-full right-full flex flex-col pt-px bg-white dark:bg-gray-800 border-x-0 border-y border-rose-600 dark:border-x-0 dark:border-y dark:border-gray-800 text-lg shadow-md overflow-hidden z-10'}
+            'transition-all w-full absolute top-full right-full flex flex-col pt-px bg-white dark:bg-gray-900 border-x-0 border-y border-rose-600 dark:border-x-0 dark:border-y dark:border-gray-800 text-lg shadow-md overflow-hidden z-10'}
         >
-            <Link to="/" className="hidden lg:block h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-700 dark:hover:bg-gray-800 dark:text-gray-50 cursor-pointer transition-colors" onClick={handleDropdown}>
+            <Link to="/" className="hidden lg:block h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-800 dark:hover:bg-gray-900 dark:text-gray-50 cursor-pointer transition-colors" onClick={handleDropdown}>
                 Home
                 <BsHouseDoor className="inline-block ml-2"/>
             </Link>
 
-            <hr className="border-1 border-rose-600 dark:border-gray-800"/>
+            <hr className="border-1 border-rose-600 dark:border-gray-900"/>
 
-            <Link to="/username" className="h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-700 dark:hover:bg-gray-800 dark:text-gray-50 cursor-pointer transition-colors" onClick={handleDropdown}>
+            <Link to="/username" className="h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-800 dark:hover:bg-gray-900 dark:text-gray-50 cursor-pointer transition-colors" onClick={handleDropdown}>
                 Profile
                 <i className="fa-regular fa-user ml-2"></i>
             </Link>
             
-            <hr className="border-1 border-rose-600 dark:border-gray-800"/>
+            <hr className="border-1 border-rose-600 dark:border-gray-900"/>
 
-            <p className="h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-700 dark:hover:bg-gray-800 dark:text-gray-50 cursor-pointer transition-colors" onClick={handleTheme}>
+            <p className="h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-800 dark:hover:bg-gray-900 dark:text-gray-50 cursor-pointer transition-colors" onClick={handleTheme}>
                 Change Theme
                 { theme === 'light' ?
                     <i className="fa-regular fa-moon ml-2"></i>
@@ -31,9 +31,9 @@ function Dropdown({ showDropdown, handleDropdown, theme, handleTheme }) {
                 }
             </p>
 
-            <hr className="border-1 border-rose-600 dark:border-gray-800"/>
+            <hr className="border-1 border-rose-600 dark:border-gray-900"/>
             
-            <p className="h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-700 dark:hover:bg-gray-800 dark:text-gray-50 cursor-pointer transition-colors">
+            <p className="h-full w-full py-3 text-center hover:bg-rose-600 hover:text-gray-50 dark:bg-gray-800 dark:hover:bg-gray-900 dark:text-gray-50 cursor-pointer transition-colors">
                 Logout
                 <i className="fa-solid fa-door-open ml-2"></i>
             </p>
