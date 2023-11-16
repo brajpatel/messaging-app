e8ce67b98ade8d0ded397184dee3d71dd3dc5e9b
brajpatel <brajeshpatel0002@gmail.com>
2023-11-16 17:27:36 +0000

    Create object url from selected image

--- src/components/ProfileEdit.jsx
+++ src/components/ProfileEdit.jsx
@@ -12,6 +12,8 @@ function ProfileEdit() {
         if(profilePicture) setProfilePicture(null);
 
         setProfilePicture(URL.createObjectURL(e.target.files[0]));
+
+        console.log(profilePicture)
     }
 
     const removeProfilePicture = () => {
@@ -19,7 +21,7 @@ function ProfileEdit() {
     }
 
     return (
-        <div className='lg:h-full w-full lg:w-auto absolute top-0 left-0 lg:relative lg:px-4 lg:py-3 flex flex-col bg-white dark:bg-gray-900 overflow-x-hidden overflow-y-auto'>
+        <div className='h-screen lg:h-full w-full lg:w-auto absolute top-0 left-0 lg:relative lg:px-4 lg:py-3 flex flex-col bg-white dark:bg-gray-900 overflow-x-hidden overflow-y-auto'>
             <div className="h-[4.4rem] md:h-[4.6rem] lg:h-auto w-full flex justify-between items-center lg:justify-center px-4 lg:py-4 font-bold bg-rose-600 lg:bg-white dark:bg-gray-800 lg:dark:bg-gray-800 text-gray-50 lg:text-gray-900 dark:text-gray-50">
                 <Link to="/username" className='block lg:hidden hover:bg-[rgb(255,255,255,0.4)] dark:hover:bg-[rgb(255,255,255,0.2)] p-2 rounded-full'>
                     <AiOutlineArrowLeft className='text-2xl cursor-pointer'/>
