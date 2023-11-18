5466fdc198b7ad6719120a4de9844dbdc3bcfec9
brajpatel <brajeshpatel0002@gmail.com>
2023-11-18 17:51:52 +0000

    Position messages and message input sections

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -1,6 +1,25 @@
+import { Link } from 'react-router-dom';
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import { IoSend } from "react-icons/io5";
+
 function ChatPage() {
     return (
-        <div>Chat page sheeesh</div>
+        <div className='h-screen w-full lg:w-auto absolute top-0 left-0 lg:relative p-2 lg:p-3 flex flex-col bg-white dark:bg-gray-900 overflow-x-hidden overflow-y-auto'>
+
+
+            <div className='h-full text-gray-900 dark:text-gray-50 p-2'>
+                Messages here
+            </div>
+
+            <div className='flex justify-center items-center gap-4 px-2 py-2'>
+                <input className='w-[80%] bg-white dark:bg-gray-800 text-lg text-black dark:text-gray-100 px-4 py-2 mt-2 border-2 border-gray-200 dark:border-gray-800 focus:outline-none focus:border-rose-500 dark:focus:border-gray-500 rounded text-center' placeholder="Type message here"/>
+
+                <button className='h-max flex justify-center items-center gap-2 bg-rose-600 hover:bg-rose-500 dark:bg-gray-800 dark:hover:bg-gray-700 px-4 py-2 text-gray-50 rounded-lg text-lg outline-none'>
+                    Send
+                    <IoSend/>
+                </button>
+            </div>
+        </div>
     )
 }
 
