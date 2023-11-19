285d6e391f58111356e0f26fb5f1c246b0ea853f
brajpatel <brajeshpatel0002@gmail.com>
2023-11-19 17:37:44 +0000

    Adjust message max widths

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -15,7 +15,7 @@ function Message({ message, dateSent, user }) {
         <div className={`flex ${user ? 'justify-end' : ''} mb-4`}>
             <div className={`w-full ${user ? 'flex flex-col items-end' : ''}`}>
                 <p className="lg:text-lg">{dateTime}</p>
-                <div className={`w-max lg:text-lg px-3 md:px-4 py-2 ${user ? 'bg-rose-600 dark:bg-rose-600 text-gray-50' : 'bg-gray-200 dark:bg-gray-800'} rounded`}>
+                <div className={`w-max max-w-[90%] md:max-w-[70%] lg:text-lg px-3 md:px-4 py-2 ${user ? 'bg-rose-600 dark:bg-rose-600 text-gray-50' : 'bg-gray-200 dark:bg-gray-800'} rounded`}>
                     {message}
                 </div>
             </div>
