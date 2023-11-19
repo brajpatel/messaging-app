d48b3cf7bf70aefd462b49bba8aa42a529dff470
brajpatel <brajeshpatel0002@gmail.com>
2023-11-19 17:37:32 +0000

    Position user messages on the right of the container

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -12,10 +12,10 @@ function Message({ message, dateSent, user }) {
     }, [])
 
     return (
-        <div className={`flex ${user ? 'justify-end' : ''} mb-2`}>
-            <div className="flex flex-col justify-end">
+        <div className={`flex ${user ? 'justify-end' : ''} mb-4`}>
+            <div className={`w-full ${user ? 'flex flex-col items-end' : ''}`}>
                 <p className="lg:text-lg">{dateTime}</p>
-                <div className={`w-4/5 lg:text-lg p-2 ${user ? 'bg-rose-600 dark:bg-rose-600 text-gray-50' : 'bg-gray-200 dark:bg-gray-800'} rounded`}>
+                <div className={`w-max lg:text-lg px-3 md:px-4 py-2 ${user ? 'bg-rose-600 dark:bg-rose-600 text-gray-50' : 'bg-gray-200 dark:bg-gray-800'} rounded`}>
                     {message}
                 </div>
             </div>
