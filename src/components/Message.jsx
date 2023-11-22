f1af2e1b410656826d2312de630594223f8d5de6
brajpatel <brajeshpatel0002@gmail.com>
2023-11-22 15:44:39 +0000

    Break long words to prevent message overflowing

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -12,10 +12,10 @@ function Message({ message, dateSent, user }) {
     }, [])
 
     return (
-        <div className={`flex ${user ? 'justify-end' : ''} mb-4`}>
+        <div className={`flex ${user ? 'justify-end' : ''} mb-4 break-words`}>
             <div className={`w-full ${user ? 'flex flex-col items-end' : ''}`}>
                 <p className="lg:text-lg mb-1">{dateTime}</p>
-                <div className={`w-max max-w-[92.5%] md:max-w-[70%] lg:text-lg px-3 md:px-4 py-2 ${user ? 'bg-rose-600 dark:bg-rose-600 text-gray-50' : 'bg-gray-200 dark:bg-gray-800'} rounded`}>
+                <div className={`w-max max-w-[87.5%] md:max-w-[70%] lg:text-lg px-3 md:px-4 py-2 ${user ? 'bg-rose-600 dark:bg-rose-600 text-gray-50' : 'bg-gray-200 dark:bg-gray-800'} rounded`}>
                     {message}
                 </div>
             </div>
