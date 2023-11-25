4551aee9470854cae36a3bb236adcd2a9b2cb397
brajpatel <brajeshpatel0002@gmail.com>
2023-11-25 12:00:29 +0000

    Map posts from an array of examples

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,36 @@
 import { useState } from "react";
 import {  IoSend } from "react-icons/io5";
+import Post from "./Post";
 
 function Feed() {
     const [postMessage, setPostMessage] = useState('');
+    const [examplePosts, setExamplePosts] = useState([
+        {
+            username: 'Ashe',
+            message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem voluptas, vel earum cumque id! Alias est maxime inventore ad.',
+            profile_picture: 'https://64.media.tumblr.com/91b159df9a8c606c4fd5a74427d2aa9c/b9116db0ab4711c9-77/s500x750/01bd6c7a8791be7f6f6e4b7afee2424cf671bd53.png',
+        },
+        {
+            username: 'Jhin',
+            message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem voluptas, vel earum cumque id! Alias est maxime inventore ad.',
+            profile_picture: 'https://i.pinimg.com/1200x/9d/00/4c/9d004c40630c28c54e1bc37d10b35657.jpg',
+        },
+        {
+            username: 'Nasus',
+            message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem voluptas, vel earum cumque id! Alias est maxime inventore ad.',
+            profile_picture: 'https://i.pinimg.com/564x/0e/d8/54/0ed8549cbf8d409bc3b90731085f865d.jpg',
+        },
+        {
+            username: 'Jinx',
+            message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem voluptas, vel earum cumque id! Alias est maxime inventore ad.',
+            profile_picture: 'https://wallpapers-clan.com/wp-content/uploads/2023/07/league-of-legends-jinx-pfp-01.jpg',
+        },
+        {
+            username: 'Irelia',
+            message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam exercitationem voluptas, vel earum cumque id! Alias est maxime inventore ad.',
+            profile_picture: 'https://i.pinimg.com/564x/fd/56/3b/fd563b26fb28a70d2b262a39af322655.jpg',
+        }
+    ])
 
     const handlePostMessage = (e) => {
         setPostMessage(e.target.value);
@@ -18,8 +46,8 @@ function Feed() {
     }
 
     return (
-        <div id="container" className='relative hidden lg:block lg:h-screen p-4 bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-50 overflow-hidden'>
-            <form className="flex justify-center items-center gap-4 px-2 py-3">
+        <div id="container" className='relative hidden lg:block lg:h-full p-4 bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-50 overflow-hidden'>
+            <form className="flex justify-center items-center gap-4 py-3">
                 <input className='w-[80%] bg-white dark:bg-gray-800 text-lg text-black dark:text-gray-100 px-4 py-2 border-2 border-gray-200 dark:border-gray-800 focus:outline-none focus:border-rose-500 dark:focus:border-gray-500 rounded' placeholder="What's on your mind today..." value={postMessage} onInput={handlePostMessage}/>
 
                 <button className='flex justify-center items-center gap-2 bg-rose-600 hover:bg-rose-500 dark:bg-gray-800 dark:hover:bg-gray-700 px-4 py-[0.85rem] sm:py-2 text-gray-50 rounded-lg text-lg outline-none' onClick={createPost} type='submit'>
@@ -27,6 +55,14 @@ function Feed() {
                     <IoSend/>
                 </button>
             </form>
+
+            <ul className="flex flex-col gap-3">
+                {examplePosts.map((post, index) => {
+                    return (
+                        <Post key={index} post={post}/>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
