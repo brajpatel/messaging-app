e9833aa11b91f5c120af9c5eca0f5658312ba6ab
brajpatel <brajeshpatel0002@gmail.com>
2023-12-13 09:40:44 +0000

    Display default profile picture to cover full background

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -12,7 +12,7 @@ function Post({ post }) {
 
     return (
         <li className="min-h-[8rem] h-full w-full rounded flex bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-800 hover:border-rose-600 dark:hover:border-gray-500 transition-all overflow-hidden select-none">
-            <div className={`relative lg:min-w-[25%] xl:min-w-[20%] bg-cover bg-center overflow-hidden`} style={{backgroundImage: `url(${post.profile_picture})`}}></div>
+            <div className={`relative lg:min-w-[25%] xl:min-w-[20%] bg-cover bg-center overflow-hidden`} style={{backgroundImage: post.profile_picture ? `url(${post.profile_picture})` : `url('/public/default-pfp.png')`}}></div>
 
             <div className="w-full flex flex-col justify-center gap-2 py-2 2xl:py-3">
                 <p className="2xl:text-[1.05rem] lg:px-[1.5rem] xl:px-3">{post.message}</p>
