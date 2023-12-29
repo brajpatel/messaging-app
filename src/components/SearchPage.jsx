32f085be52430fcf7b2edd92ce3416b75c6ae39a
brajpatel <brajeshpatel0002@gmail.com>
2023-12-29 11:02:53 +0000

    Increase search bar width on larger screens

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -53,7 +53,7 @@ function SearchPage() {
                     <p className='hidden lg:block lg:text-xl xl:text-2xl lg:underline'>Search for friends</p>
                 </div>
 
-                <div className='w-[90%] sm:w-3/5 md:w-1/2 xl:w-1/3 mx-auto flex justify-center items-center py-4 lg:p-2'>
+                <div className='w-[90%] sm:w-3/5 md:w-1/2 xl:w-[40%] mx-auto flex justify-center items-center py-4 lg:p-2'>
                     <input className='w-full bg-white dark:bg-gray-800 text-lg text-black dark:text-gray-100 px-4 py-2 mt-2 border-2 border-gray-200 dark:border-gray-800 focus:outline-none focus:border-rose-500 dark:focus:border-gray-500 rounded text-center' placeholder="Search by username" onInput={handleSearch}/>
                 </div>
             </div>
